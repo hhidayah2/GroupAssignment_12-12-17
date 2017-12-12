ea06044f2c681ab0bbe5a46338b5a57d8f364f1b
Damia <Damia@Acer_Aspire>
2017-12-12 09:52:34 +0800

    add myutils

---
+++ BookShop/src/java/coreservlets/utils/MyUtils.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package coreservlets.utils;
+
+/**
+ *
+ * @author hisham
+ */
+import java.sql.Connection;
+ 
+import javax.servlet.ServletRequest;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+ 
+import coreservlets.*;
+ 
+public class MyUtils {
+ 
+    public static final String ATT_NAME_CONNECTION = "ATTRIBUTE_FOR_CONNECTION";
+ 
+    private static final String ATT_NAME_USER_NAME = "ATTRIBUTE_FOR_STORE_USER_NAME_IN_COOKIE";
+ 
+    // Store Connection in request attribute.
+    // (Information stored only exist during requests)
+    public static void storeConnection(ServletRequest request, Connection conn) {
+        request.setAttribute(ATT_NAME_CONNECTION, conn);
+    }
+ 
+    // Get the Connection object has been stored in attribute of the request.
+    public static Connection getStoredConnection(ServletRequest request) {
+        Connection conn = (Connection) request.getAttribute(ATT_NAME_CONNECTION);
+        return conn;
+    }
+ 
+    
+ 
+    //Store booking info in session
+    public static void storebooking(HttpSession session,String room,String date) {
+        // On the JSP can access via ${room}
+        session.setAttribute("room",room);
+         session.setAttribute("date",date);
+    }
+     public static void storebooking(HttpSession session,int id) {
+    session.setAttribute("id",id);
+    }
+    
+ public static int getId(HttpSession session) {
+        int id = (int) session.getAttribute("id");
+        return id;
+    }
+ 
+    
+ 
+    // Delete cookie.
+    public static void deleteUserCookie(HttpServletResponse response) {
+        Cookie cookieUserName = new Cookie(ATT_NAME_USER_NAME, null);
+        // 0 seconds (This cookie will expire immediately)
+        cookieUserName.setMaxAge(0);
+        response.addCookie(cookieUserName);
+    }
+ 
+}
